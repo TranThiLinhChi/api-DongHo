f6e3fef070d336b49e7f86fb83fb2f7d457a0009
TranThiLinhChi <70497465+TranThiLinhChi@users.noreply.github.com>
2020-10-22 20:01:27 +0700

    add data

---
+++ Model/AuthenticateModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Model
+{
+    public class AuthenticateModel
+    {
+        [Required]
+        public string username { set; get; }
+        [Required]
+        public string password { set; get; }
+    }
+}
---
+++ Model/Thang.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    public class Thang
+    {
+        public string thang1 { set; get; }
+        public string thang2 { set; get; }
+        public string thang3 { set; get; }
+        public string thang4 { set; get; }
+        public string thang5 { set; get; }
+        public string thang6 { set; get; }
+        public string thang7 { set; get; }
+        public string thang8 { set; get; }
+        public string thang9 { set; get; }
+        public string thang10 { set; get; }
+        public string thang11 { set; get; }
+        public string thang12 { set; get; }
+
+    }
+}
